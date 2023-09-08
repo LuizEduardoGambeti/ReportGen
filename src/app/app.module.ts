0808bff1b79b9ee860885d5fc2d44a315ca8a6bf
Lucas Coelho <49642902+lucasCoelho245@users.noreply.github.com>
2023-09-08 18:35:32 -0300

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginCadastroComponent } from './login/login-cadastro/login-cadastro.component';
+import { LoginFormComponent } from './login/login-form/login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginCadastroComponent
+    LoginCadastroComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
