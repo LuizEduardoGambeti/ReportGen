beb4f411067a412c38e79559e7432631fbc77c83
Lucas Coelho <49642902+lucasCoelho245@users.noreply.github.com>
2023-10-30 17:26:43 -0300

    updates dynamics

---
+++ src/app/dynamics-components/dynamic-data/components-data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComponentsDataService {
+  constructor(private http: HttpClient) { }
+
+  public getData(): Observable<any[]> {
+    return this.http.get<any[]>('assets/data.json'); // Caminho relativo ao arquivo JSON
+  }
+}
