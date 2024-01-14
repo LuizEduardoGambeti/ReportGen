c4fcd3f2435a20845a06d5c867fdb7419ac71119
Gambeti <70177236+LuizEduardoGambeti@users.noreply.github.com>
2024-01-14 13:50:22 -0300

    FIX: Menu foi arrumado e o design modificado (#1)
    Co-authored-by: Gambeti <gambeti@gmail.com>

--- src/app/dynamics-components/dashboard-card/dashboard-card.component.ts
+++ src/app/dynamics-components/dashboard-card/dashboard-card.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-card',
@@ -9,5 +10,8 @@ export class DashboardCardComponent {
   @Input() title!: string;
   @Input() cardIcon!: string;
   @Output() cardClicked = new EventEmitter<string>();
+
+  constructor(private router: Router) {
+  }
 
 }
